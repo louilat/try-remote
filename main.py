3a7711b3e3725f750d2e14a5e3f3083786e74f80
louilat <lmm.latournerie@gmail.com>
2025-01-29 16:43:15 +0100

    debug

--- main.py
+++ main.py
@@ -13,37 +13,38 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 TOKEN = os.environ["TOKEN"]
 IP_ADDRESS = os.environ["IPA"]
 
+print(IP_ADDRESS)
 
-w3 = Web3(Web3.HTTPProvider(IP_ADDRESS))
+# w3 = Web3(Web3.HTTPProvider(IP_ADDRESS))
 
-print("Is connected: ", w3.is_connected())
+# print("Is connected: ", w3.is_connected())
 
-block_number = w3.eth.get_block_number()
+# block_number = w3.eth.get_block_number()
 
-df = pd.DataFrame({
-    "col1": [block_number]
-})
+# df = pd.DataFrame({
+#     "col1": [block_number]
+# })
 
-buffer = io.StringIO()
+# buffer = io.StringIO()
 
-df.to_csv(buffer, index=False)
+# df.to_csv(buffer, index=False)
 
-s3_client = boto3.client(
-    "s3",
-    endpoint_url = 'https://'+'minio.lab.sspcloud.fr',
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY,
-    aws_session_token=TOKEN,
-)
+# s3_client = boto3.client(
+#     "s3",
+#     endpoint_url = 'https://'+'minio.lab.sspcloud.fr',
+#     aws_access_key_id=ACCESS_KEY,
+#     aws_secret_access_key=SECRET_KEY,
+#     aws_session_token=TOKEN,
+# )
 
-s3_client.put_object(
-    Body=buffer.getvalue(),
-    Bucket="llatournerie",
-    Key="exp-remote/data.csv"
-)
+# s3_client.put_object(
+#     Body=buffer.getvalue(),
+#     Bucket="llatournerie",
+#     Key="exp-remote/data.csv"
+# )
 
 
-ip = get('https://api.ipify.org').content.decode('utf8')
-print('My public IP address is: {}'.format(ip))
+# ip = get('https://api.ipify.org').content.decode('utf8')
+# print('My public IP address is: {}'.format(ip))
 
-print("Success")
+# print("Success")
